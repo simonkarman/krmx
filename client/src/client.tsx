04e402215bbed952683555430dd7d3e03bf3c720
Simon Karman <mail@simonkarman.nl>
2024-03-30 10:49:57 +0100

    Ensured that useKrmx cannot be called without a KrmxProvider and improved the sdk documentation of the Krmx client

--- client/src/client.tsx
+++ client/src/client.tsx
@@ -24,16 +24,11 @@ type FromServerMessage = RejectedMessage | AcceptedMessage | JoinedMessage |
 interface ResetAction { type: 'reset', payload: { username: string } }
 type Action = ResetAction | FromServerMessage;
 
-export type KrmxState = {
+type KrmxState = {
   username: string;
   rejectionReason?: string;
   isLinked: boolean;
   users: { [username: string]: { isLinked: boolean}};
-};
-const initialState: KrmxState = {
-  username: '',
-  isLinked: false,
-  users: {},
 };
 const reducer = (_state: KrmxState, action: Action) => {
   return produce<KrmxState, KrmxState>(_state, state => {
@@ -76,32 +71,85 @@ const reducer = (_state: KrmxState, action: Action) => {
 };
 
 type MessageConsumer = <TMessage extends { type: string }>(message: TMessage) => void;
-type KrmxContextProps = {
+
+/**
+ * The KrmxContextProps type describes the fields and methods that are accessible by using the useKrmx hook.
+ */
+export type KrmxContextProps = {
+  /**
+   * Indicates whether the KrmxProvider was able to successfully set up a websocket connection to the Krmx Server.
+   */
   isConnected: boolean,
+  /**
+   * When invoked, makes the KrmxProvider retry setting up a websocket connection to the Krmx Server.
+   *
+   * By default, it will only attempt to reconnect if there is not already a connection to the Krmx server. If the force argument is set to true, it
+   *  will even reconnect if it is currently already connected to the Krmx server. In that case, the current connection will be dropped first.
+   */
   reconnect: (force?: boolean) => void,
+  /**
+   * When invoked, makes the KrmxProvider link the connection to a user.
+   *
+   * @param username The username of the user you want to link to. For example: 'simon'.
+   * @param auth An optional auth string. This value is sent to the Krmx server and will be available in the `on('authenticate', ...)` listener.
+   */
   link: (username: string, auth?: string) => void,
+  /**
+   * When invoked, makes the KrmxProvider send a message to the server.
+   */
   send: MessageConsumer,
+  /**
+   * When invoked, makes the KrmxProvider unlink the connection from its user.
+   */
   unlink: () => void,
+  /**
+   * When invoked, makes the KrmxProvider have the user leave the server.
+   */
   leave: () => void,
+  /**
+   * Lets a component subscribe to incoming events from the Krmx server.
+   *
+   * @param messageConsumer The event handler that will be invoked every time that the Krmx server sends a message to this client. The event handler
+   *                         is invoked with the message as the first parameter. Keep in mind that messages internal to Krmx, such as 'krmx/join' or
+   *                         'krmx/leave' messages, are *NOT* emitted to this event handler.
+   * @param deps The reactive dependency array (similar to useEffect). To avoid errors, you always have to pass an empty dependency array (aka []).
+   *              More information: https://simonkarman.github.io/krmx/krmx-api/client#reactive-values-in-usemessages-event-handler
+   */
   useMessages: (messageConsumer: MessageConsumer, deps: React.DependencyList) => void,
 } & KrmxState;
-const KrmxContext = createContext<KrmxContextProps>({
-  isConnected: false,
-  reconnect: () => {},
-  isLinked: false,
-  username: '',
-  rejectionReason: undefined,
-  users: {},
-  link: () => {},
-  send: () => {},
-  unlink: () => {},
-  leave: () => {},
-  useMessages: () => {},
-});
+
+const KrmxContext = createContext<KrmxContextProps | undefined>(undefined);
+
+/**
+ * The useKrmx hook allows a component to use the Krmx connection provided by a KrmxProvider component in the tree above it. When no KrmxProvider can
+ *  be found, this function will throw an Error.
+ *
+ * Example: `const { isLinked, link, username } = useKrmx();`
+ *
+ * To find out which properties are returned by the useKrmx hook, please refer to the `KrmxContextProps` type.
+ */
 export const useKrmx = function (): KrmxContextProps {
-  return useContext(KrmxContext);
+  const ctx = useContext(KrmxContext);
+  if (ctx === undefined) {
+    throw new Error('useKrmx cannot be called without a KrmxProvider');
+  }
+  return ctx;
 };
 
+/**
+ * The KrmxProvider component sets up a connection to a Krmx Server and exposes the usage of this connection to all components in the tree below it.
+ *  The child components will be able to use the Krmx context. For example to access whether the connection is successful, to link or unlink the
+ *  connection from a user, and/or to send and receive messages.
+ *
+ * Example: `<KrmxProvider serverUrl={"ws://localhost:1234"}><MyComponentUsingKrmx /></KrmxProvider>`
+ *
+ * To use Krmx state in your components, please refer to the useKrmx hook.
+ *
+ * @param props The props define how the connection to the Krmx Server should be setup. Currently, the only property you can provide is the
+ *               `serverUrl`. For the serverUrl property you have to provide the ws:// or wss:// url of the websocket endpoint where your Krmx server
+ *               is running. For example: `ws://my-subdomain.example.org:3002/my-game` or `ws://localhost:1234`.
+ * @constructor
+ */
 export const KrmxProvider: FC<PropsWithChildren<{
   serverUrl: string,
 }>> = (props) => {
@@ -110,12 +158,17 @@ export const KrmxProvider: FC<PropsWithChildren<{
 
   const [connectionAttempt, setConnectionAttempt] = useState(0);
   const [status, setStatus] = useState<'waiting' | 'open' | 'closed'>('waiting');
+  const [state, dispatch] = useReducer(reducer, {
+    username: '',
+    isLinked: false,
+    users: {},
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [state, dispatch] = useReducer(reducer, initialState as unknown as any);
+  } as unknown as any);
 
   const useMessages = (messageConsumer: MessageConsumer, deps: React.DependencyList) => {
     if (deps === undefined || deps.length !== 0) {
-      throw new Error('useMessages must be used with an empty dependency list (aka \'[]\'), consult the Krmx documentation for more information');
+      const docs = 'https://simonkarman.github.io/krmx/krmx-api/client#reactive-values-in-usemessages-event-handler';
+      throw new Error(`useMessages must be used with an empty dependency list (aka '[]'), consult the documentation for more information: ${docs}.`);
     }
     const onMessage = useCallback(messageConsumer, deps);
     useEffect(() => {
