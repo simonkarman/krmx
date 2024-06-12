35867f07f0d4acb4b287e0e737a8ada989b992e2
Simon Karman <mail@simonkarman.nl>
2024-06-12 12:31:22 +0200

    Added ability to pass @krmx/client Props to @krmx/client-react creation too

--- client-react/src/client.tsx
+++ client-react/src/client.tsx
@@ -1,27 +1,45 @@
 import { useSyncExternalStore } from 'react';
-import { Client, createClient } from '@krmx/client';
+import { Client, createClient, Props } from '@krmx/client';
 
-export type Listener = () => void;
+type Listener = () => void;
+
+/**
+ * The state that is returned by the `useKrmx` hook.
+ */
 export type ClientState = {
   status: ReturnType<Client['getStatus']>,
   users: ReturnType<Client['getUsers']>,
   username: ReturnType<Client['getUsername']>,
 };
 
-export const createClientReact = () => {
+/**
+ * Create a client and a hook to use the Krmx Client in a React component.
+ *
+ * @param props The properties with which to create the Client.
+ * @returns An object with the Krmx Client (`client`) and a hook to use the Krmx Client in a React component (`useKrmx`).
+ */
+export function createClientReact(props?: Props) {
   // Create client
-  const client = createClient();
+  const client = createClient(props);
   client.all((eventName) => {
     if (eventName !== 'message') { emit(); }
   });
 
-  // Create listeners
-  let listeners: Listener[] = [];
+  // Create state and listeners
   let state: ClientState = {
     status: 'initializing',
     users: client.getUsers(),
     username: client.getUsername(),
   };
+  let listeners: Listener[] = [];
+  function subscribe(listener: Listener) {
+    listeners = [...listeners, listener];
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    };
+  }
+
+  // Create emitter
   function emit() {
     state = {
       status: client.getStatus(),
@@ -30,14 +48,8 @@ export const createClientReact = () => {
     };
     listeners.forEach(l => l());
   }
-  function subscribe(listener: Listener) {
-    listeners = [...listeners, listener];
-    return () => {
-      listeners = listeners.filter(l => l !== listener);
-    };
-  }
 
-  // Create hooks
+  // Create useKrmx hook
   const useKrmx = () => {
     return useSyncExternalStore(subscribe, () => state, () => ({
       status: 'initializing',
@@ -46,13 +58,17 @@ export const createClientReact = () => {
     } as ClientState));
   };
 
+  // Return the client and the useKrmx hook
   return {
     useKrmx,
     client,
   };
-};
+}
 
 // TODO: add this back in as a generic createReactClientStore store
+// export function createReactClientStore(client: Client) {
+//   return undefined;
+// }
 // export const createLatestMessagesStore = (client: Client, numberOfMessages: number) => {
 //   let listeners: Listener[] = [];
 //   type State = { id: string, message: Message }[];
